ada8fc51f949ab0ac65b868ced8f9e586d03d7a4
Abhay <chauhan27.abhay@gmail.com>
2020-09-07 19:39:21 +0530

    Updates

---
+++ precache-manifest.e99567fad4ae0bdd9974cdcd433bf15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1a782bbfa287ea84169dfb730ea258",
+    "url": "/Github-Jobs/index.html"
+  },
+  {
+    "revision": "9b3d264780c97d1e6a26",
+    "url": "/Github-Jobs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "908ba6846d58025f4ef4",
+    "url": "/Github-Jobs/static/js/2.073642e1.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Github-Jobs/static/js/2.073642e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3d264780c97d1e6a26",
+    "url": "/Github-Jobs/static/js/main.fe5a568f.chunk.js"
+  },
+  {
+    "revision": "177d7e4a38dce7509a40",
+    "url": "/Github-Jobs/static/js/runtime-main.7f0b6df5.js"
+  }
+]);
